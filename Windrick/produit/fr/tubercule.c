6e6bf03bf0f49044bb73190e69400b04239cc995
WindRk511 <tchelminowindrick@gmail.com>
2025-09-06 22:54:52 +0300

    tubercule

---
+++ Windrick/produit/fr/tubercule.c
@@ -0,0 +1,24 @@
+#include"prod.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+prod* tubercule() 
+{
+	int n = 4;
+	prod* tub = malloc(n*sizeof(prod));
+
+	strcpy(tub[0]->nom,"Pomme de terre");
+	tub[0]->type = 6;
+	tub[0]->sol[0] = 0;
+	tub[0]->mois[3] = 1;
+	tub[0]->mois[0] = 2;
+	tub[0]->mois[1] = 3;
+	tub[0]->mois[1] = 4;
+	tub[0]->mois[1] = 5;
+	tub[0]->mois[1] = 10;
+	tub[0]->mois[1] = 11;
+	tub[0]->mois[2] = 12;
+	tub[0]->sais[0] = 1;
+	tub[0]->sais[0] = 2;
+}
