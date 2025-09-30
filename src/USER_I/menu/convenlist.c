8160e33521863320b93a537ad06d23b417e5fc41
WindRk511 <tchelminowindrick@gmail.com>
2025-09-30 18:40:51 +0300

    menu-1.0.1

--- src/USER_I/menu/convenlist.c
+++ src/USER_I/menu/convenlist.c
@@ -5,57 +5,62 @@
 void lcmois (prond* p) 
 {
 	printf("Choisir le mois:\n");
-	printf("Janvier\n");
-	printf("Fevrier\n");
-	printf("Mars\n");
-	printf("Avril\n");
-	printf("Mai\n");
-	printf("Juin\n");
-	printf("Juillet\n");
-	printf("Août\n");
-	printf("Septembre\n");
-	printf("Octobre\n");
-	printf("Novembre\n");
-	printf("Décembre\n");
+	printf("1) Janvier\n");
+	printf("2) Fevrier\n");
+	printf("3) Mars\n");
+	printf("4) Avril\n");
+	printf("5) Mai\n");
+	printf("6) Juin\n");
+	printf("7) Juillet\n");
+	printf("8) Août\n");
+	printf("9) Septembre\n");
+	printf("10) Octobre\n");
+	printf("11) Novembre\n");
+	printf("12) Décembre\n");
+
+	
 }
 
 void lctype ()
 {
 	printf("Choisir le type de produit:\n");
-	printf("Cereale\n" );
-        printf("Legumineux\n" );
-        printf("Fruit\n" );
-        printf("Legume\n" );
-        printf("Tubercule\n" );
-        printf("Racine\n");
-        printf("INDEFINIT\n");
+	printf("1) Cereale\n" );
+        printf("2) Legumineux\n" );
+        printf("3) Fruit\n" );
+        printf("4) Legume\n" );
+        printf("5) Tubercule\n" );
+        printf("6) Racine\n");
+        printf("7) INDEFINIT\n");
 }
 
 void lcsol ()
 {
 	printf("Choisir le type de sol:\n");
-	 printf("Sol sableux,\n");
-         printf("Sol argileux,\n");
-         printf("Sol limoneux,\n");
-         printf("Sol humifère (ou organique),\n");
-         printf("humide\n");
-         printf("Aquatique\n");
-         printf("Meuble\n");
-         printf("Riche\n");
-         printf("Vaseux\n");
-         printf("Léger\n");
-         printf("Drainé\n");
-         printf("INDEFINIT,\n");
+	 printf("1) Sol sableux,\n");
+         printf("2) Sol argileux,\n");
+         printf("3) Sol limoneux,\n");
+         printf("4) Sol humifère (ou organique),\n");
+         printf("5) humide\n");
+         printf("6) Aquatique\n");
+         printf("7) Meuble\n");
+         printf("8) Riche\n");
+         printf("9) Vaseux\n");
+         printf("10) Léger\n");
+         printf("11) Drainé\n");
+         printf("12) INDEFINIT,\n");
 }
 
 void lcsaison () 
 {
 	printf("Choisir la saison :\n");
- 	printf("froid,\n");
-        printf("chaud,\n");
-        printf("pluie,\n");
-        printf("sec,\n");
-        printf("tempéré,\n");
-        printf( "INDEFINIT,\n");
+ 	printf("2) froid,\n");
+        printf("2) chaud,\n");
+        printf("2) pluie,\n");
+        printf("2) sec,\n");
+        printf("2) tempéré,\n");
+        printf( "2) INDEFINIT,\n");
 }
+
+
+
 
