ce154d126b181b933b037e84ee3c470485724de3
WindRk511 <tchelminowindrick@gmail.com>
2025-11-18 10:20:35 +0100

    11/18/25

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 projet :
 Creation programme a but de proposer des cultures à cultuver chaque mois ou chaque saison pour mieux choisir un bon semence et ameliorer les rendement
+
+# https://github.com/n3co-team/Agricu_Mad.git
--- test/comm.sh
+++ test/comm.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:../lib"
-gcc -g main.c -I../headers -L../lib  -lmenu.so -lanal.so -lconven.so -lbdprod.so  
+gcc -g main.c -I../headers -L../lib  -lmenu -lanal -lconven -lbdprod  
