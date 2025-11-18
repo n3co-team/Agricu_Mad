b3070f19abc11259c5d0924d873b61da2e579c4f
WindRk511 <tchelminowindrick@gmail.com>
2025-11-18 19:42:36 +0100

    11/18/25

---
+++ puf1.sh
@@ -0,0 +1,22 @@
+set -e
+
+CURRENT_BRANCH=$(git branch | grep "^\*" | cut -d" " -f2)
+
+if [[ $1 == "lo" ]]
+then
+	git reset --hard origin/${CURRENT_BRANCH}
+elif [[ $1 == "di" ]]
+then
+	git push --force-with-lease
+fi
+
+read -rp "Voullez vous pousser vers github votre nouveau modification ?"
+read -p "Message de validation : " message
+if [[ -z ${message} ]] 
+then
+	message=$(date +%D)
+fi
+	git add .
+	git rm --cached $0
+	git commit -m "${message}"
+	git push
