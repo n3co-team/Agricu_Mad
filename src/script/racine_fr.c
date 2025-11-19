7b938c62e17c9fd690a84165ed13a7d65cff4761
WindRk511 <tchelminowindrick@gmail.com>
2025-11-19 19:05:57 +0100

    11/19/25

--- src/script/racine_fr.c
+++ src/script/racine_fr.c
@@ -21,7 +21,7 @@ prod* racine()
 		tub[i].ntp=NBR;
 	}
 
-	strcy(tub[0].nom,"CAROTTE");
+	strcpy(tub[0].nom,"CAROTTE");
 	tub[0].type=6;
 	tub[0].sol[0]=10;
 	tub[0].sol[1]=1;
@@ -33,7 +33,7 @@ prod* racine()
 	tub[0].mois[3]=6;
 	tub[0].mois[4]=7;
 
-	strcy(tub[1].nom,"PANAIS");
+	strcpy(tub[1].nom,"PANAIS");
 	tub[1].type=6;
 	tub[1].sol[0]=4;
 	tub[1].sais[0]=5;
@@ -41,14 +41,14 @@ prod* racine()
 	tub[1].mois[1]=4;
 	tub[1].mois[2]=5;
 
-	strcy(tub[2].nom,"CÉLERI-RAVE");
+	strcpy(tub[2].nom,"CÉLERI-RAVE");
 	tub[2].type=6;
 	tub[2].sol[0]=8;
 	tub[2].sais[0]=3;
 	tub[2].sais[1]=5;
 	tub[2].mois[0]=5;
 
-	strcy(tub[3].nom,"BETTERAVE");
+	strcpy(tub[3].nom,"BETTERAVE");
 	tub[3].type=6;
 	tub[3].sol[0]=10;
 	tub[3].sol[1]=8;
@@ -59,7 +59,7 @@ prod* racine()
 	tub[3].mois[1]=5;
 	tub[3].mois[2]=6;
 
-	strcy(tub[4].nom,"RADIS");
+	strcpy(tub[4].nom,"RADIS");
 	tub[4].type=6;
 	tub[4].sol[0]=10;
 	tub[4].sais[0]=1;
@@ -72,7 +72,7 @@ prod* racine()
 	tub[4].mois[4]=6;
 	tub[4].mois[5]=7;
 
-	strcy(tub[5].nom,"RUTABAGA");
+	strcpy(tub[5].nom,"RUTABAGA");
 	tub[5].type=6;
 	tub[5].sol[0]=4;
 	tub[5].sais[0]=2;
@@ -81,7 +81,7 @@ prod* racine()
 	tub[5].mois[0]=5;
 	tub[5].mois[1]=6;
 
-	strcy(tub[6].nom,"NAVET");
+	strcpy(tub[6].nom,"NAVET");
 	tub[6].type=6;
 	tub[6].sol[0]=7;
 	tub[6].sol[1]=4;
@@ -94,7 +94,7 @@ prod* racine()
 	tub[6].mois[3]=7;
 	tub[6].mois[4]=8;
 
-	strcy(tub[7].nom,"PERSIL");
+	strcpy(tub[7].nom,"PERSIL");
 	tub[7].type=6;
 	tub[7].sol[0]=6;
 	tub[7].sais[0]=10;
