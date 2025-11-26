aa74551b551757685c9dab5504c68ffc28382455
dominique503 <ramoravelonajeandominique@gmail.com>
2025-11-26 22:02:08 +0300

    m

--- src/dom.c
+++ src/dom.c
@@ -184,7 +184,7 @@ void afficher_menu_principal() {
     if (scanf("%d", &choix_cultures) == 1) {
 	if(choix_cultures > 0 && choix_cultures <= nombre_cultures){
 	   afficher_menu_regions();
-	   initialiser_cultures();
+//	   initialiser_cultures();
         } 
     else if(choix_cultures == 0){
       return;
