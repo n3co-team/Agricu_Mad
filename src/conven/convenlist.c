c9a20a346745923c1d268cfb9c96bf6bd1837d76
dominique503 <ramoravelonajeandominique@gmail.com>
2025-12-10 21:07:58 +0300

    m

--- src/conven/convenlist.c
+++ src/conven/convenlist.c
@@ -86,7 +86,7 @@ void lctype(prod *p)
     p->type = choix_car(1, 6);
     if (p->type == 1) {
 //	afficher_menu_principal();
-	cereale();
+	cereale(p);
 	initialiser_cultures();
 //	prod* fruit();
 	//exit (0);
