8591c57d0d2ab61ef43ccb5f3b30cf51ac7949a1
dominique503 <ramoravelonajeandominique@gmail.com>
2025-12-10 16:58:11 +0300

    m

--- src/dom.c
+++ src/dom.c
@@ -280,7 +280,7 @@ void afficher_informations_cultures(int culture_choix, int region_choix)
     int culture_index = culture_choix - 1;
     int region_index = region_choix - 1;
     printf("\n¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤\n");
-    printf("\t\t\n\033[34m ===>MOMBAMOMBAN'NY\033[0m %s \n", cultures[culture_index].nom);
+    printf("\t\n\t\033[34m ===>MOMBAMOMBAN'NY\033[0m%s \n", cultures[culture_index].nom);
     printf("\t\033[34m ===>FARITRA\033[0m: %s\n\n", regions[region_index].nom);
     
     //printf("Code: %s\n", cultures[culture_index].code);
@@ -292,11 +292,11 @@ void afficher_informations_cultures(int culture_choix, int region_choix)
     // Vérifier la compatibilité région/culture
     if (est_region_compatible(culture_choix, region_choix)) {
 	printf
-	    ("\t\n \033[34m===>METY AMINY\033[0m : Voarindra tsara amin'ny toetran'ny faritra io voly  %s io\n",
+	    ("\t\n\t\033[34m===>METY AMINY\033[0m : Voarindra tsara amin'ny toetran'ny faritra io voly  %s io\n",
 	     regions[region_index].nom);
     } else {
 	printf
-	    ("\t\n \033[34m===>TSY METY AMINY \033[0m : Tsy dia mety tsara amin'io faritra io ito voly  %s ito\n",
+	    ("\t\n\t\033[34m===>TSY METY AMINY \033[0m : Tsy dia mety tsara amin'io faritra io ito voly  %s ito\n",
 	     regions[region_index].nom);
     }
     printf("\n¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤¤\n");
