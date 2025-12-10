c5dd1cc7f93164dd542884538389886b7c151001
WindRk511 <tchelminowindrick@gmail.com>
2025-12-11 00:08:09 +0100

    Update

--- src/USER_I/resultat/infoprod.c
+++ src/USER_I/resultat/infoprod.c
@@ -35,7 +35,7 @@ void resultf(lprod** donne) {
 	printf("\nMisafidiana laharana\n ");
 	choix=choix_car(1,i);
 	choix = choix - 1;
-	printf("choix=%d\n",choix)
+	printf("choix=%d\n",choix);
 	temp = *donne;
 	i=0;
 	while (1) {
